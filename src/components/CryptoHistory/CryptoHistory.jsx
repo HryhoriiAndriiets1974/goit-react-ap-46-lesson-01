460d5427329f04b9bbbcedcce78f145b33664876
HryhoriiAndriiets1974 <ander_1_agg@ukr.net>
2022-07-03 16:23:42 +0300

    lesson-01-task-04

---
+++ src/components/CryptoHistory/CryptoHistory.jsx
@@ -0,0 +1,27 @@
+import { BaseTable, THead, Th, Tr, Td } from './CryptoHistory.styled';
+
+
+export const CryptoHistory = ({items}) => {
+  return (
+  <BaseTable>
+    <THead>
+      <tr>
+        <Th>№</Th>
+        <Th>PRICE</Th>
+        <Th>AMOUNT</Th>
+        <Th>DATE</Th>
+      </tr>
+    </THead>
+
+    <tbody>
+    {items.map(({id, price, amount, date}) => (
+          <Tr key={id}>
+            <Td>{id}</Td>
+            <Td>{price}</Td>
+            <Td>{amount}</Td>
+            <Td>{date}</Td>
+          </Tr>
+    ))}
+    </tbody>
+  </BaseTable>)
+};
